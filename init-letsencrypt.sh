4ab204d8196ddad3be07142267b9bf074d8d2705
Brad Reeves <reevesbra@outlook.com>
2022-05-03 03:39:28 +0000

    server stuffs

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(protego-app.xyz www.protego-app.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="protego.contact@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -29,6 +29,7 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
+
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
